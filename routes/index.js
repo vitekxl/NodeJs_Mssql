d7f4b7b315f87f9d00e6503e321ad490f57d378c
vitekxl <17123vitek@gmail.com>
2019-08-27 17:16:59 +0200

    add runnig add sql commands

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var bodyParser = require('body-parser');
-
 var sql = require("mssql");
+var request = require('request');
+var app = express();
+
 
 var config = {
   user: 'sa',
@@ -10,7 +11,6 @@ var config = {
   server: 'localhost',
   database: 'TestDB'
 };
-
 
 
 function objToArr(arr){
@@ -22,7 +22,6 @@ function objToArr(arr){
   }
   var head = Object.keys(arr[0]);
   var values = [];
-  var columns = new Array(head.length);
 
   arr.forEach( function (obj) {
     values.push(Object.values(obj));
@@ -32,50 +31,6 @@ function objToArr(arr){
   res.values = values;
 
   return res;
-}
-
-if (!Object.prototype.watch) {
-  Object.defineProperty(Object.prototype, "watch", {
-    enumerable: false
-    , configurable: true
-    , writable: false
-    , value: function (prop, handler) {
-      var
-          oldval = this[prop]
-          , newval = oldval
-          , getter = function () {
-            return newval;
-          }
-          , setter = function (val) {
-            oldval = newval;
-            return newval = handler.call(this, prop, oldval, val);
-          }
-      ;
-
-      if (delete this[prop]) { // can't watch constants
-        Object.defineProperty(this, prop, {
-          get: getter
-          , set: setter
-          , enumerable: true
-          , configurable: true
-        });
-      }
-    }
-  });
-}
-
-// object.unwatch
-if (!Object.prototype.unwatch) {
-  Object.defineProperty(Object.prototype, "unwatch", {
-    enumerable: false
-    , configurable: true
-    , writable: false
-    , value: function (prop) {
-      var val = this[prop];
-      delete this[prop]; // remove accessors
-      this[prop] = val;
-    }
-  });
 }
 
 const poolPromise = new sql.ConnectionPool(config)
@@ -139,9 +94,10 @@ router.get('/r', async  (req, res, next) => {
 });
 
 
-async function RetrieveFromDb(sqlReq)
+async function RetrieveFromDb(sqlReq, res)
 {
   try {
+    console.log("-> " + sqlReq);
     const pool = await poolPromise;
     const result = await pool.request()
         .query(sqlReq);
@@ -157,10 +113,11 @@ async function RetrieveFromDb(sqlReq)
 
 
 router.post('/r', async (req, res) => {
-
+  console.log("R POST");
+  console.log(req.body);
   var input = {};
   input.recordset = {};
-  input.tablename = req.body['table'];
+  input.tablename = req.body['tableName'];
   input.type = "";
 
   let table = {};
@@ -170,33 +127,8 @@ router.post('/r', async (req, res) => {
   table.ma = [];
   table.geraetID = [];
   table.colName = [];
-
-  input.watch('recordset', function (id, oldval, newval) {
-    var arr = {};
-
-    if (newval.length === 0) {
-      arr.head = [];
-      arr.values = [];
-      table.colName.forEach(function (colName) {
-        arr.head.push(colName);
-      })
-      //console.log(arr)
-    } else {
-      arr = objToArr(newval);
-    }
-   // console.log(table);
-    res.render('enterValues',
-        {
-          href: 'stylesheets/table.css',
-          title: 'tible',
-          tablename: input.tablename,
-          head: arr.head,
-          values: arr.values,
-          table: table
-        });
-    return newval;
-  });
-
+  table.zustandName = [];
+  table.tart = [];
 
   var sqlReq1 = 'Select ARTIKEL_ART_NAME From ARTIKEL_ART';
   var sqlReq2 = 'Select ARTIKEL_NAME From ARTIKEL';
@@ -204,63 +136,318 @@ router.post('/r', async (req, res) => {
   var sqlReq4 = 'Select MA_VORNAME, MA_NACHNAME From MITARBEITER';
   var sqlReq5 = 'Select GERAET_ID From GERAET';
   var sqlReq6 = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='" + input.tablename+ "';";
+  var sqlReq7 = "SELECT ZUSTAND_NAME FROM ZUSTAND";
+  var sqlReq8 = "SELECT T_NAME FROM TRANSAKTIONSART";
 
-    RetrieveFromDb(sqlReq1).then(function (data) {
+  var promises = [];
+
+    promises.push(RetrieveFromDb(sqlReq1, res).then(function (data) {
       data.recordset.forEach(function (art) {
         table.artikelArt.push(art['ARTIKEL_ART_NAME']);
       })
-    });
+    }));
 
-    RetrieveFromDb(sqlReq2).then(function (data) {
+  promises.push(RetrieveFromDb(sqlReq2, res).then(function (data) {
       data.recordset.forEach(function (art) {
         table.artikel.push(art['ARTIKEL_NAME']);
       })
-    });
+    }));
 
-  RetrieveFromDb(sqlReq3).then(function (data) {
+  promises.push(RetrieveFromDb(sqlReq3, res).then(function (data) {
     data.recordset.forEach(function (art) {
       table.firma.push(art['FIRMA_NAME']);
     })
-  });
+  }));
 
-  RetrieveFromDb(sqlReq4).then(function (data) {
+  promises.push(RetrieveFromDb(sqlReq4, res).then(function (data) {
     data.recordset.forEach(function (art) {
       table.ma.push(art['MA_VORNAME'] + " " + art['MA_NACHNAME'] );
     })
-  });
+  }));
 
-  RetrieveFromDb(sqlReq5).then(function (data) {
+  promises.push(RetrieveFromDb(sqlReq5, res).then(function (data) {
     data.recordset.forEach(function (art) {
       table.geraetID.push(art['GERAET_ID'] );
     })
-  });
+  }));
 
-  RetrieveFromDb(sqlReq6).then(function (data) {
+  promises.push(RetrieveFromDb(sqlReq6, res).then(function (data) {
     data.recordset.forEach(function (art) {
       table.colName.push(art['COLUMN_NAME'] );
     })
-  });
+  }));
+
+  promises.push(RetrieveFromDb(sqlReq7, res).then(function (data) {
+    data.recordset.forEach(function (art) {
+      table.zustandName.push(art['ZUSTAND_NAME'] );
+    })
+  }));
+
+  promises.push(RetrieveFromDb(sqlReq8, res).then(function (data) {
+    data.recordset.forEach(function (art) {
+      table.tart.push(art['T_NAME'] );
+    })
+  }));
 
 
   var sqlReq = 'Select * From ' + input.tablename + ";";
-  RetrieveFromDb(sqlReq).then(function (f) {
+  console.log(sqlReq);
+  promises.push(RetrieveFromDb(sqlReq, res).then(function (f) {
        input.recordset = f.recordset;
+  }));
+
+  Promise.all(promises).then(function () {
+    var arr = {};
+
+    if ( input.recordset.length === 0) {
+      arr.head = [];
+      arr.values = [];
+      table.colName.forEach(function (colName) {
+        arr.head.push(colName);
+      })
+      //console.log(arr)
+    } else {
+      arr = objToArr(input.recordset );
+    }
+    // console.log(table);
+    res.render('enterValues',
+        {
+          href: 'stylesheets/table.css',
+          title: 'table',
+          tablename: input.tablename,
+          head: arr.head,
+          values: arr.values,
+          table: table
+        });
   });
 
 });
 
 
-router.post('/confirmation', function(req, res, next) {
-  res.render('confirmation', { href: 'stylesheet/confirmation.css' });
+async function makeTransaction(sqlReq, res, req){
+  console.log("make=> " + sqlReq)
+  try {
+    const pool = await poolPromise;
+    const transaction = pool.transaction();
+    return transaction.begin().then(function () {
+          pool.request().query(sqlReq).then(function (){
+            transaction.commit(error => {
+              if(error) throw error;
+              else console.log("commit")
+            });
+          }
+      );
+    });
+  } catch (err) {
+    res.status(500);
+    res.send(err.message)
+  }
+}
+
+router.post('/confirmation', async (req, res, next) => {
   console.dir(req.body);
 
   var table = req.body.tableName;
+  let values = undefined;
+  var sqlreq = "INSERT INTO %table_name%(%column%) VALUES (%values%)";
 
-  if(table === 'ZUSTAND' && req.body.zustandName !== '' ){
+  sqlreq = sqlreq.replace(/%table_name%/, table);
 
+  if(table === 'TEST_TABLE'){
+    sqlreq = sqlreq.replace(/%column%/, "TEXT");
+    values = "'" + req.body.text.trim() +"'";
+  }else if(table === 'ARTIKEL_ART'){
+    sqlreq = sqlreq.replace(/%column%/, "ARTIKEL_ART_NAME");
+    values = "'" + req.body.artikelArtName.trim() +"'";
+  }else if(table === 'ZUSTAND'){
+    sqlreq = sqlreq.replace(/%column%/, "ZUSTAND_NAME");
+    values = "'" + req.body.zustandName.trim() +"'";
+  }else if(table === 'TRANSAKTIONSART'){
+    sqlreq = sqlreq.replace(/%column%/, "T_NAME");
+    values = "'" + req.body.transart.trim() +"'";
+  }else if(table === 'FIRMA'){
+    sqlreq = sqlreq.replace(/%column%/, "FIRMA_NAME");
+    values = "'" + req.body.firmaName.trim() +"'";
+  }else if(table === "ARTIKEL"){
+    sqlreq = sqlreq.replace(/%column%/, "ARTIKEL_ART_ID, ARTIKEL_NAME, ANZAHL_AUF_LAGER");
+
+    let aasql = "Select ARTIKEL_ART_ID from ARTIKEL_ART WHERE ARTIKEL_ART_NAME = '" + req.body.artikelArt + "';";
+    var id = undefined;
+    await RetrieveFromDb(aasql, res)
+        .then( function (result) {
+          id = result.recordset[0]['ARTIKEL_ART_ID']
+          values = "" + id + ", '"+ req.body.artikelName.trim() + "', "+ req.body.anzahl.trim();
+        })
+  }else if(table === "GERAET") {
+    sqlreq = sqlreq.replace(/%column%/, "GERAET_ID, ARTIKEL_ID, ZUSTAND_ID, IST_AUF_LAGER");
+
+    let artikelsql = "SELECT ARTIKEL_ID FROM ARTIKEL WHERE ARTIKEL_NAME='" + req.body.artikelName + "';";
+    let artikelartsql = "Select ARTIKEL_ART_ID from ARTIKEL_ART WHERE ARTIKEL_ART_NAME = '" + req.body.artikelArt + "';";
+    let zustandsql = "SELECT ZUSTAND_ID FROM ZUSTAND WHERE ZUSTAND_NAME='" + req.body.zustandName + "';";
+
+    let artikelartId = undefined;
+    let artikelId = undefined;
+    let zustandID = undefined;
+    let aufLager = req.body.isAufLager ? 1 : 0;
+
+
+    await RetrieveFromDb(artikelartsql, res)
+        .then(function (result) {
+          artikelartId = result.recordset[0]['ARTIKEL_ART_ID'];
+        });
+
+    await RetrieveFromDb(artikelsql, res)
+        .then(function (result) {
+          artikelId = result.recordset[0]['ARTIKEL_ID'];
+        });
+
+    await RetrieveFromDb(zustandsql, res)
+        .then(function (result) {
+          zustandID = result.recordset[0]['ZUSTAND_ID'];
+        });
+
+
+    values = "'" + req.body.geraet_ID.trim() + "', " + artikelId + ", " + zustandID + ", " + aufLager;
+
+  }else if(table === 'MITARBEITER'){
+    sqlreq = sqlreq.replace(/%column%/, "K_NUMMER, MA_VORNAME, MA_NACHNAME, FIRMA_ID");
+
+    let firmasql = "SELECT FIRMA_ID FROM FIRMA WHERE FIRMA_NAME='" + req.body.firmaName + "';";
+    let firmaID = undefined;
+
+    await RetrieveFromDb(firmasql, res)
+        .then(function (result) {
+          firmaID = result.recordset[0]['FIRMA_ID'];
+        });
+    values = "'" + req.body.maID + "', '" + req.body.maVname + "', '" + req.body.maNname + "', " + firmaID;
+
+  }else if(table === 'TRANSAKTION'){
+    sqlreq = sqlreq.replace(/%column%/, "TRANSAKTION_ART_ID, MA_1, MA_2, GERAET_ID, IST_AKTUEL, BESCHREIBUNG");
+
+
+    let ma1name     = req.body.ma1name.trim();
+    let ma2name     = req.body.ma2name.trim();
+    let geratId     = req.body.gerateName;
+    let description = req.body.descr.trim();
+    let isActual    = req.body.isActual ? 1 : 0;
+
+    if(ma2name === '[NULL]') ma2name = undefined;
+    ma1name = ma1name.split(' ');
+    if(ma2name) ma2name = ma2name.split(' ');
+
+    let tartId = undefined;
+    let tartsql = "SELECT T_ART_ID FROM TRANSAKTIONSART WHERE T_NAME='" + req.body.tart + "';";
+    let masql = "SELECT K_NUMMER FROM MITARBEITER WHERE MA_VORNAME='%VNAME%' AND MA_NACHNAME='%NNAME%';";
+
+    let ma1Id = undefined;
+    let ma2Id = "NULL";
+
+    console.log(ma1name);
+
+      await RetrieveFromDb(tartsql, res)
+          .then(function (result) {
+            tartId = result.recordset[0]['T_ART_ID'];
+          });
+
+      let ma1_sql = masql.replace(/%VNAME%/, ma1name[0] ).replace(/%NNAME%/,ma1name[1]);
+      await RetrieveFromDb(ma1_sql, res)
+          .then(function (result) {
+            ma1Id = result.recordset[0]['K_NUMMER'];
+            ma1Id = "'" + ma1Id + "'";
+          });
+      if(ma2name){
+        let ma2_sql = masql.replace(/%VNAME%/, ma2name[0] ).replace(/%NNAME%/,ma2name[1]);
+        await RetrieveFromDb(ma2_sql, res)
+            .then(function (result) {
+              ma2Id = result.recordset[0]['K_NUMMER'];
+              ma2Id = "'" + ma2Id + "'";
+            });
+      }
+      values = "" + tartId + ", " + ma1Id+ ", " + ma2Id + ", '" + geratId+ "', " + isActual + ", '" + description +"'";
   }
-  //res.send('what are you doing here?');
+  else {
+    res.status(500);
+    res.send("unrecognised table")
+    return;
+  }
+
+  res.render("confirmation");
+  sqlreq = sqlreq.replace(/%values%/, values);
+  //await console.log("-> " + sqlreq);
+  await makeTransaction(sqlreq,res ,req);
+
+/*  var json = {'tableName' : 'FIRMA'};
+  request.post({
+    url: 'http://localhost:3000/r',
+    body: json,
+    json: true
+  }, function (err, res, body) {
+
+  })*/
+
 });
 
+router.get('/sql', async (req, res) => {
+  res.render('runSql', {text : ""})
+});
+
+router.post('/sql', async (req, res) => {
+  console.log(req.body);
+
+  let request = req.body.sqlrequest;
+  let message = "";
+
+  let firstword = request.split(' ');
+
+  let result = undefined;
+  const pool = await poolPromise;
+
+  if(firstword.length > 0 && firstword[0].toLowerCase() === 'select'){
+    try {
+      message += "=> " + request + " <br> ";
+      result = await pool.request()
+          .query(request);
+      result = result.recordset;
+      message += "OK! <br>";
+      message += "result: <br> ";
+
+
+      let resmessage = JSON.stringify(result);
+      resmessage = resmessage.replace(/},{/g, "},<br>&emsp;{");
+      resmessage = resmessage.replace(/{"/g, "{ \"");
+      resmessage = resmessage.replace(/":/g, "\": ");
+      resmessage = resmessage.replace(/,"/g, ",\" ");
+      resmessage = resmessage.replace(/}/g, " }");
+      message += resmessage;
+
+    } catch (err) {
+      message += "ERROR: <br> ";
+      message +=  err.message;
+    }
+  }else {
+    try {
+      const transaction = pool.transaction();
+      await transaction.begin().then(function () {
+        pool.request().query(request).then(function (){
+              transaction.commit(error => {
+                if(error) throw error;
+                else console.log("commit")
+              });
+              message += "OK! <br>";
+
+            }
+        );
+      });
+    } catch (err) {
+      message += "ERROR: <br> ";
+      message +=  err.message;
+    }
+  }
+
+
+  res.render('runSql', {answer: message})
+
+
+});
 
 module.exports = router;
