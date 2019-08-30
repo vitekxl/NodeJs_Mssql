7c84e3fd24afa7d3ea8beb2676fa9539e9194588
bilousov <17123vitek@gmail.com>
2019-08-30 15:53:15 +0200

    add sql request running. Convert result of request from obj to arr. Add foreign keys in tables

--- db/DB.js
+++ db/DB.js
@@ -1,11 +1,11 @@
 var sql = require("mssql");
 
 class DB{
-     constructor(config) {
 
+     constructor(config) {
          this.tables = [];
          this.tables.names = [];
-         this.poolPromise = new sql.ConnectionPool(config)
+         this._poolPromise = new sql.ConnectionPool(config)
              .connect()
              .then(pool => {
                  console.log('Connected to MSSQL')
@@ -26,7 +26,7 @@ class DB{
                  throw new Error("error name of Table: "+tableName);
              }
 
-             for (const cname of table.columnsName){
+             for (const cname of table.keys){
                  if(!table[cname].isActual){
                     await this.loadColumn(tableName, cname);
                  }
@@ -43,14 +43,14 @@ class DB{
                  await this.loadColumn(tableName, columnName);
              }
              return column.values;
-         }
+         };
 
          this.loadTable = async (tableName) =>{
              var table = this.tables[tableName];
-             var sql = 'SELECT * FROM %TABLE%'.replace(/%TABLE%/, tableName);
+             var sql = `SELECT * FROM ${tableName};`;
 
              if(typeof table === 'undefined'){
-               //  throw new Error("error name of Table: "+tableName+" or column: " + columnName);
+                 throw new Error("error name of Table: "+tableName+" or column: " + columnName);
              }
 
              await this.RetrieveFromDb(sql).then(function (result) {
@@ -66,64 +66,144 @@ class DB{
              });
          };
 
-         this.loadColumn = async (tableName, columnName) => {
-             var table = this.tables[tableName];
-             var sql = 'SELECT %COLUMN% FROM %TABLE%'.replace(/%TABLE%/, tableName).replace(/%COLUMN%/, columnName);
+         this.loadColumn = async (tableName, key) => {
+             let table = this.tables[tableName];
+             let sql = `SELECT ${key} FROM ${tableName};`
 
              if(typeof table === 'undefined'){
-               //  throw new Error("error name of Table: "+tableName+" or column: " + columnName);
+                 throw new Error("error name of Table: "+tableName+" or column: " + columnName);
              }
              await this.RetrieveFromDb(sql).then(function (result) {
+                 let cnt = 0;
                  result.recordset.forEach(function (set) {
-                     table[columnName].values.push(set[columnName]);
+                     table[key].values.push(set[key]);
+                     cnt++;
                  });
-                 table[columnName].isActual = true;
+                 table.rowsCnt = cnt;
+                 table[key].isActual = true;
              });
+         };
+
+         this.selectRequest =   async (slqRequest) => {
+             var res= undefined;
+             await this.RetrieveFromDb(slqRequest).then(function (result) {
+                 res = result.recordset;
+             });
+             return res;
          }
+
+         this.getColumns = async (tableName, keys) =>{
+             var sql = `Select ${keys.join(", ")} from ${tableName}`;
+             return this.selectRequest(sql);
+         }
+
+         this.transpose = async (rows) => {
+             if(rows.length === 0 )return undefined;
+            // let table = _this.tables[tableName];
+             let res = { keys:Object.keys(rows), values: [] };
+
+             for (const set of rows){
+                 res.values.push(Object.values(set));
+             }
+             return res;
+         };
+
+         this.insertRequest= async (table, keys, values) => {
+             var sql  = `INSERT INTO ${table}${keys} VALUES ${values};`;
+             await this.makeTransaction(sql);
+         }
+
+          this.makeTransaction = async (sqlReq) =>{
+
+             console.log("make=> " + sqlReq)
+
+             const pool = await this._poolPromise;
+             const transaction = pool.transaction();
+             return transaction.begin().then(function () {
+                 pool.request().query(sqlReq).then(function (){
+                         transaction.commit(error => {
+                             if(error) throw error;
+                             else console.log("commit")
+                         });
+                     }
+                 );
+             });
+
+         }
+
     }
 
     async init(config){
-        let sqlReq = "Select TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='%DBNAME%';"
-        sqlReq = sqlReq.replace(/%DBNAME%/, config.database);
+        let sqlReq = `Select TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='${config.database}';`
+
         var tables = {};
-        var tablesNames = [];
         var _this = this;
 
-
          await _this.RetrieveFromDb(sqlReq).then(function(result) {
-            result.recordset.forEach(function (column) {
-                tables[column['TABLE_NAME']] = { columnsName: [], foreignColName : []};
-                tablesNames.push(column['TABLE_NAME']);
+            result.recordset.forEach(function (tableNameSet) {
+                var tableName = tableNameSet['TABLE_NAME'];
+                tables[tableName] = { keys: [], foreignKeys : [], primaryKey: "", ownKeys: [], rowsCnt: undefined};
+
             })
         }).then(async () => {
              // console.log(Object.keys(tables));
              for (const table of Object.keys(tables)) {
-                 let getAllInfo = "sp_help '%Table_Name%'".replace(/%Table_Name%/, table);
+                 let getAllInfo = `sp_help '${table}'`
 
                  await _this.RetrieveFromDb(getAllInfo).then(function (data) {
 
-                     let colnames = [];
-
                      for (const set of data.recordsets[1]) {
-                         colnames.push(set["Column_name"]);
+                         tables[table].keys.push(set["Column_name"]);
+                         tables[table][set["Column_name"]] = {values: [], isActual: false};
                      }
                      for (const set of data.recordsets[6]) {
                          if (set["constraint_type"] === 'FOREIGN KEY') {
-                             colnames = colnames.filter(function (value) {
-                                 return value !== set["constraint_keys"];
-                             });
-                             tables[table].foreignColName.push(set["constraint_keys"]);
+                             tables[table].foreignKeys.push(set["constraint_keys"]);
+                         }
+                         else if(set["constraint_type"].match('PRIMARY KEY')){
+                             tables[table].primaryKey = set["constraint_keys"];
                          }
                      }
-                     // console.log(tables)
-                     for (const cname of colnames) {
-                         tables[table][cname] = {values: [], isActual: false};
-                         tables[table].columnsName.push(cname);
-                     }
+                     tables[table].ownKeys = tables[table].keys.filter(function (value) {
+                         return !tables[table].foreignKeys.includes(value);
+                     })
+
                  });
+
              }
              _this.tables = tables;
-            // _this.tables.names = tablesNames;
+         }).then( async () => {
+
+             for (const tableName of Object.keys(_this.tables)) {
+                 _this.tables[tableName].getColumn = function(columnName){
+                     return _this.getColumn(tableName, columnName);
+                 };
+
+                 _this.tables[tableName].transpose = async () => {
+
+                     let table = _this.tables[tableName];
+                     let res = { keys:table.keys, values: [] };
+
+                     if(table.rowsCnt === 0){
+                         return res;
+                     }
+
+                     for (let i = 0; i < table.rowsCnt; i++) {
+                         res.values.push([]);
+                     }
+
+                     for (let j = 0; j < res.keys.length; j++) {
+                         for (let i = 0; i < table.rowsCnt; i++) {
+                             res.values[i].push(
+                                 table[res.keys[j]].values[i]
+                             );
+                         }
+                     }
+
+                     return res;
+                 }
+             }
+
          })
 
     }
@@ -131,7 +211,7 @@ class DB{
     async RetrieveFromDb(sqlReq, res){
         try {
             console.log("-> " + sqlReq);
-            const pool = await this.poolPromise;
+            const pool = await this._poolPromise;
             const result = await pool.request()
                 .query(sqlReq);
             return result;
